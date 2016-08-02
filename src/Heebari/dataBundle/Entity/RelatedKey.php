dce4a9acba539bfafe2c04eecf6f291297d8b6a4
Landry <Kateulandry@icloud.com>
2016-08-02 10:38:58 +0200

    premierpost

---
+++ src/Heebari/dataBundle/Entity/RelatedKey.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Heebari\dataBundle\Entity;
+
+use Doctrine\ORM\Mapping AS ORM;
+
+/**
+ * @ORM\Entity
+ */
+class RelatedKey
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Heebari\dataBundle\Entity\Motclef", inversedBy="relatedKey")
+     * @ORM\JoinColumn(name="motclef_id", referencedColumnName="id")
+     */
+    private $motclef;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return RelatedKey
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set motclef
+     *
+     * @param \Heebari\dataBundle\Entity\Motclef $motclef
+     *
+     * @return RelatedKey
+     */
+    public function setMotclef(\Heebari\dataBundle\Entity\Motclef $motclef = null)
+    {
+        $this->motclef = $motclef;
+
+        return $this;
+    }
+
+    /**
+     * Get motclef
+     *
+     * @return \Heebari\dataBundle\Entity\Motclef
+     */
+    public function getMotclef()
+    {
+        return $this->motclef;
+    }
+}
